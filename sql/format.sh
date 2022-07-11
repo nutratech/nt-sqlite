b8f312a20f1b0ba4eda2b0878bf6143a9f35e975
Shane Jaroch <chown_tee@proton.me>
2022-07-11 19:18:27 -0400

    wip tables
    use rst for changelog, no plural tables, wip other
    
    fix test, in Makefile
    
    tweak changelog

--- sql/format.sh
+++ sql/format.sh
@@ -3,9 +3,8 @@
 cd "$(dirname "$0")"
 
 # Format
-pg_format -L -s 2 -w 80 tables.sql >tables.fmt.sql
+pg_format -L -s 2 -w 100 tables.sql >tables.fmt.sql
 mv tables.fmt.sql tables.sql
 
-pg_format -L -s 2 -w 80 functions.sql >functions.fmt.sql
+pg_format -L -s 2 -w 100 functions.sql >functions.fmt.sql
 mv functions.fmt.sql functions.sql
-
