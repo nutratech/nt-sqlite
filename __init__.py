47c6db2a418db690ba1b8e407756240ec2f296f3
Shane Jaroch <chown_tee@proton.me>
2022-07-12 16:48:24 -0400

    sp

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 """
 NOTE: this is used as a submodule in the cli repo (ntclient)
- so we need this to all be packagable, e.g. have __init__.py files
+ so we need this to all be packageable, e.g. have __init__.py files
 """
